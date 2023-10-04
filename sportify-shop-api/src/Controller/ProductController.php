f4b8b30c655bbec9a76999dd3de5b5aa5a671446
Ericcost <eric.costerousse@gmail.com>
2023-10-04 10:55:50 +0200

    ProductController: Index Route + option category filter

--- sportify-shop-api/src/Controller/ProductController.php
+++ sportify-shop-api/src/Controller/ProductController.php
@@ -2,41 +2,49 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Product;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
-use Symfony\Component\HyypFoundation\Request;
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/api/products', name: 'api_product_')]
+#[Route('/products', name: 'product_')]
 class ProductController extends AbstractController
 {
-    #[Route('/index', name: 'index', methods: ['GET'])]
-    public function index(ManagerRegistry $doctrine): JsonResponse
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(Request $request, SerializerInterface $serializer, ManagerRegistry $doctrine): JsonResponse
     {
+        $category_name = $request->query->get('category_name');
         $products = $doctrine->getRepository(Product::class)->findAll();
-
+        print('-------------'.$category_name.'-------------');
         $data = [];
 
         foreach ($products as $product) {
+
+            $availability = ($product->getQuantity() !== null && $product->getQuantity() > 0);
+
             $data[] = [
                 'id' => $product->getId(),
                 'name' => $product->getName(),
-                'description' => $product->getDescription(),
+                'description' => $product->getPrice(),
                 'price' => $product->getDescription(),
                 'quantity' => $product->getQuantity(),
-                'availability' => $product->getAvailability(),
+                'availability' => $availability,
                 'gender' => $product->getGender(),
                 'image_slug' => $product->getImageSlug(),
-                'category_id' => $product->getCategoryId(),
-                'sub_category_id' => $product->getSubCategoryId(),
-                'created_at' => $product->getCreatedAt->getCreatedAt(),
+                'category_id' => $product->getCategory()->getId(),
+                'sub_category_id' => $product->getSubCategory()->getId(),
+                'created_at' => $product->getCreatedAt(),
             ];
         }
+        // VarDumper::dump($data);
+        $json = $serializer->serialize($data, 'json');
 
-        return $this->json($data);
+        return new JsonResponse($json, 200, [], true);
     }
 
     // #[Route('/{id}', name: 'show', methods: ['GET'])]
