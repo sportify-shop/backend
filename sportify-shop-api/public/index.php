e4e35fd339681cde58a0cfe5732ccdeb3d2dfbc5
Ludovic Sbr <sobrero.ludovic@gmail.com>
2023-10-05 14:37:10 +0200

    chore: editing public/index.php

--- sportify-shop-api/public/index.php
+++ sportify-shop-api/public/index.php
@@ -2,6 +2,17 @@
 
 use App\Kernel;
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header("Allow: *");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === "OPTIONS") {
+    die();
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
