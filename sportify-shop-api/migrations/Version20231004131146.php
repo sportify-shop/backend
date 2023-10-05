85856ccb1ed2f1a5e3ec456813b2b9659dc6a468
Ericcost <eric.costerousse@gmail.com>
2023-10-05 10:52:53 +0200

    new db-init

--- sportify-shop-api/migrations/Version20231004131146.php
+++ sportify-shop-api/migrations/Version20231004131146.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231004072543 extends AbstractMigration
+final class Version20231004131146 extends AbstractMigration
 {
     public function getDescription(): string
     {
